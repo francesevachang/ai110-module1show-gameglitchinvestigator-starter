c5df0ea611f6595d4e671e16155b39706162c4e4
Frances Chang <francesevachang@gmail.com>
2026-06-23 03:01:15 -0400

    fixed bug: handled guess out of range edge case

--- app.py
+++ app.py
@@ -96,14 +96,14 @@ if st.session_state.status != "playing":
     st.stop()
 
 if submit:
-    st.session_state.attempts += 1
-
     ok, guess_int, err = parse_guess(raw_guess)
 
     if not ok:
-        st.session_state.history.append(raw_guess)
         st.error(err)
+    elif guess_int < low or guess_int > high:
+        st.error(f"Out of range! Pick a number between {low} and {high}.")
     else:
+        st.session_state.attempts += 1
         st.session_state.history.append(guess_int)
 
         #FIX: fixed returned value of check_guess in agent mode so tests work properly
